42c6727c88c0594e49d160a2d050ee97a1536765
henrique-carvalhos <henriqueart7@gmail.com>
2023-02-11 20:19:28 -0300

    e

--- PetShop.Application/Queries/GetAddress/GetAddressQuery.cs
+++ PetShop.Application/Queries/GetAddress/GetAddressQuery.cs
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-﻿using MediatR;
+using MediatR;
 using PetShop.Application.ViewModels;
-using System;
-=======
-﻿using System;
->>>>>>> a87d793ea8b2780862709b87271a4f1d9d25b8cc
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PetShop.Application.Queries.GetAddress
 {
-<<<<<<< HEAD
+
     public class GetAddressQuery : IRequest<AddressViewModel>
-=======
-    public class GetAddressQuery
->>>>>>> a87d793ea8b2780862709b87271a4f1d9d25b8cc
     {
         public GetAddressQuery(int id)
         {
--- PetShop.Application/Queries/GetAddress/GetAddressQueryHandler.cs
+++ PetShop.Application/Queries/GetAddress/GetAddressQueryHandler.cs
@@ -1,29 +1,17 @@
-<<<<<<< HEAD
-﻿using MediatR;
+using MediatR;
 using PetShop.Application.ViewModels;
 using PetShop.Core.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
->>>>>>> a87d793ea8b2780862709b87271a4f1d9d25b8cc
 using System.Threading.Tasks;
 
 namespace PetShop.Application.Queries.GetAddress
 {
-<<<<<<< HEAD
     public class GetAddressQueryHandler : IRequestHandler<GetAddressQuery, AddressViewModel>
     {
         private readonly IAddressRepository _addressRepository;
         public GetAddressQueryHandler(IAddressRepository addressRepository)
         {
-            _addressRepository = addressRepository; 
+            _addressRepository = addressRepository;
         }
         public async Task<AddressViewModel> Handle(GetAddressQuery request, CancellationToken cancellationToken)
         {
@@ -41,9 +29,5 @@ namespace PetShop.Application.Queries.GetAddress
 
             return addressViewModel;
         }
-=======
-    public class GetAddressQueryHandler
-    {
->>>>>>> a87d793ea8b2780862709b87271a4f1d9d25b8cc
     }
 }
